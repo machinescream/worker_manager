e8b5fe241acaca2966e8dc4e40fc6de16392e37c
Renesanse <dsrenesanse@gmail.com>
2019-11-22 09:12:13 +0300

    added initialization cold start flow

--- lib/scheduler.dart
+++ lib/scheduler.dart
@@ -18,21 +18,23 @@ abstract class Scheduler {
 class _RegularScheduler extends Scheduler {
   @override
   void manageQueue() {
-    if (queue.isNotEmpty) {
-      final availableWorker = isolates
-          .firstWhere((worker) => !worker.isBusy && worker.isInitialized, orElse: () => null);
-      if (availableWorker != null) {
-        final task = queue.removeAt(0);
-        availableWorker.work(task: task).listen((result) {
-          result is ErrorResult
-              ? task.completer.completeError(result.error)
-              : task.completer.complete(result.asValue.value);
-          manageQueue();
-        });
-      }
-      if (isolates.where((isolate) => !isolate.isBusy && !isolate.isInitialized).length ==
-          isolates.length) {
-        _warmUpFirst().then((_) => manageQueue());
+    // initialization flow for cold start
+    if (isolates.where((isolate) => !isolate.isBusy && !isolate.isInitialized).length ==
+        isolates.length) {
+      _warmUpFirst().then((_) => manageQueue());
+    } else {
+      if (queue.isNotEmpty) {
+        final availableWorker = isolates
+            .firstWhere((worker) => !worker.isBusy && worker.isInitialized, orElse: () => null);
+        if (availableWorker != null) {
+          final task = queue.removeAt(0);
+          availableWorker.work(task: task).listen((result) {
+            result is ErrorResult
+                ? task.completer.completeError(result.error)
+                : task.completer.complete(result.asValue.value);
+            manageQueue();
+          });
+        }
       }
     }
   }
