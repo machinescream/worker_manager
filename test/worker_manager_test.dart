c0aee67c28df76e8bc33de1da1a60c95fe5856f9
dsurnin <dsurnin@ozon.ru>
2020-01-12 18:33:25 +0300

    edited readme

--- test/worker_manager_test.dart
+++ test/worker_manager_test.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:worker_manager/executor.dart';
 
 void main() {
   test('adding stress test', () async {
     final list = [];
-    final tasks = List.generate(3, (index) => Task(function: fib, arg: 40));
+    final tasks = List.generate(3, (index) => Task(function: fib, arg: 6));
     tasks.forEach((task) {
       Executor().addTask(task: task).listen((data) {
         list.add(data);
@@ -21,12 +23,12 @@ void main() {
 
   test('fifo test', () async {
     final result = <int>[];
-    final task1 = Task(function: fib, arg: 10);
+    final task1 = Task(function: fib, arg: 6);
     Executor().addTask(task: task1).listen((data) {
       result.add(data);
     });
     task1.cancel();
-    final task2 = Task(function: fib, arg: 30);
+    final task2 = Task(function: fib, arg: 6);
     Executor().addTask(task: task2).listen((data) {
       result.add(data);
     });
@@ -36,9 +38,9 @@ void main() {
   });
 }
 
-int fib(int n) {
-  if (n < 2) {
-    return n;
+int fib(number) {
+  if (number < 2) {
+    return number;
   }
-  return fib(n - 2) + fib(n - 1);
+  return fib(number - 2) + fib(number - 1);
 }
