db3e12918b72d2204bd3f641a21a8762d7627a41
Renesanse <dsrenesanse@gmail.com>
2020-05-31 18:16:38 +0300

    added test

---
+++ test/tests.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:worker_manager/src/executor.dart';
+
+void main() async {
+  test('1', () async {
+    await Executor().warmUp();
+    final result = await Executor().execute(arg1: 20, fun1: fib).next((value) {
+      throw value;
+    }).catchError((e) {
+      return -1;
+    });
+  });
+}
+
+int fib(int n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 2) + fib(n - 1);
+}
