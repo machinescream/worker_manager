bafceadada5bccdc79b65492ea0d701dcfdab021
Renesanse <dsrenesanse@gmail.com>
2021-09-22 18:01:34 +0300

    https://github.com/Renesanse/worker_manager/issues/43

--- lib/worker_manager.dart
+++ lib/worker_manager.dart
@@ -3,3 +3,4 @@ library worker_manager;
 export 'src/cancelable/cancelable.dart';
 export 'src/scheduling/executor.dart';
 export 'src/cancelable/cancel_token.dart';
+export 'src/scheduling/task.dart' show WorkPriority;
