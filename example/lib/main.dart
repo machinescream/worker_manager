3aae9293333b0b14e553243595a5cc53d3ada8c8
Moaz El-sawaf <moazelsawaf@hotmail.com>
2022-09-30 04:09:29 +0200

    Fixed the issues of Dart Analyzer That was causing the Dart Action Workflow to fail

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
       // Performance overlay throws unimplemented for Flutter Web
       // showPerformanceOverlay: true,
       debugShowCheckedModeBanner: false,
@@ -29,7 +28,7 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
