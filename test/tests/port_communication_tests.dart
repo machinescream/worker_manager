0a5244b6f61d6bd7d64fdeeaafd7f5882b5b6a2e
Renesanse <dsrenesanse@gmail.com>
2023-05-08 17:28:39 +0200

    refactor: port communication fix

--- test/tests/port_communication_tests.dart
+++ test/tests/port_communication_tests.dart
@@ -4,9 +4,9 @@ import 'package:worker_manager/worker_manager.dart';
 void portCommunicationTests() {
   test("port communication", () async {
     late int message;
-    await workerManager.executeWithPort(
+    await workerManager.executeWithPort<void, int>(
       (port) => port.send(1),
-      onMessage: (m) => message = m as int,
+      onMessage: (m) => message = m,
     );
     expect(message, 1);
   });
