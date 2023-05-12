646fc0c4f45b997bc21a3c682e02969730b144d8
narumi <56487872+narumi147@users.noreply.github.com>
2023-05-12 15:57:55 +0800

    Fix web import

--- lib/worker_manager.dart
+++ lib/worker_manager.dart
@@ -11,7 +11,7 @@ import 'package:meta/meta.dart';
 import 'package:uuid/uuid.dart';
 import 'package:worker_manager/src/cancelable/cancelable.dart';
 import 'package:worker_manager/src/number_of_processors/processors_io.dart'
-    if (dart.library.web) 'package:worker_manager/src/number_of_processors/processors_web.dart';
+    if (dart.library.html) 'package:worker_manager/src/number_of_processors/processors_web.dart';
 import 'package:worker_manager/src/scheduling/task.dart';
 import 'package:worker_manager/src/scheduling/work_priority.dart';
 import 'package:worker_manager/src/worker/worker.dart';
