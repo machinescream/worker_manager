fe999b803a31ae4824a1487111b682111b4b284c
Eugene Makar <yauhen@yandex-team.ru>
2024-02-13 22:32:11 +0300

    update example android embedding to the latest supported one

--- example/android/app/src/main/kotlin/com/renesanse/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/renesanse/example/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.renesanse.example
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine)
   }
 }
