08baacf4df7fa03dfb49da3791ecc6b636d03ca0
Mack Myers <mack@pieces.app>
2024-07-01 22:27:14 -0400

    WASM Support

--- lib/src/port/send_port.dart
+++ lib/src/port/send_port.dart
@@ -1,1 +1,1 @@
-export 'send_port_io.dart' if (dart.library.html) 'send_port_web.dart';
+export 'send_port_io.dart' if (dart.library.html) 'send_port_web.dart' if (dart.library.js) 'send_port_web.dart';
